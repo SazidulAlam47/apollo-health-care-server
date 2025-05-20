edb1ba2463dde59f31aaa13f129f11368b4a7fa2
Sazidul Alam <sazidulalam20@gmail.com>
2025-05-20 20:42:35 +0600

    update global error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,21 +4,32 @@ import { ErrorRequestHandler } from 'express';
 import status from 'http-status';
 import config from '../config';
 import { ZodError } from 'zod';
-import { TErrorResponse } from '../interfaces';
+import { Prisma } from '../../../generated/prisma';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     let statusCode: number = err?.statusCode || status.INTERNAL_SERVER_ERROR;
     let message: string = err?.message || 'Something went wrong';
+    let error = err;
 
-    if (err instanceof ZodError) {
-        statusCode = status.BAD_REQUEST;
+    if (
+        err instanceof ZodError ||
+        err instanceof Prisma.PrismaClientValidationError
+    ) {
+        statusCode = status.UNPROCESSABLE_ENTITY;
         message = 'Validation Error';
+    }
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+            statusCode = status.CONFLICT;
+            message = 'Duplicate Error';
+            error = err.meta;
+        }
     }
 
     res.status(statusCode).json({
         success: false,
-        message: message,
-        error: err,
+        message,
+        error,
         stack: config.NODE_ENV === 'development' ? err?.stack : undefined,
     });
 };
