fef4c0b53025162965a3f7c4dee7b266f4cd555c
Sazidul Alam <sazidulalam20@gmail.com>
2025-05-21 19:43:31 +0600

    convert DataTime

---
+++ src/app/utils/convertDataTime.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const convertDataTimeToUTC = (date: Date) => {
+    const offset = date.getTimezoneOffset() * 60000;
+
+    console.log(date.getHours()); // converted to local time from DB
+    console.log(date.getUTCHours()); // original time from DB
+
+    return new Date(date.getTime() + offset);
+};
+
+export default convertDataTimeToUTC;
