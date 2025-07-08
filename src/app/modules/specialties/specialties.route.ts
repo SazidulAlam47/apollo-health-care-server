32482de9531d065e5d622bc9ecba0d93a825022e
Sazidul Alam <sazidulalam20@gmail.com>
2025-07-08 10:40:23 +0600

    feat: add authentication middleware to specialties routes for secure access

--- src/app/modules/specialties/specialties.route.ts
+++ src/app/modules/specialties/specialties.route.ts
@@ -3,6 +3,7 @@ import { SpecialtiesControllers } from './specialties.controller';
 import { SpecialtiesValidations } from './specialties.validation';
 import { upload } from '../../utils/sendImageToCloudinary';
 import validateRequestWithFileCleanup from '../../middlewares/validateRequestWithFileCleanup';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
@@ -10,11 +11,16 @@ router.get('/', SpecialtiesControllers.getAllSpecialties);
 
 router.post(
     '/',
+    auth('ADMIN', 'SUPER_ADMIN'),
     upload.single('file'),
     validateRequestWithFileCleanup(SpecialtiesValidations.createSpecialties),
     SpecialtiesControllers.createSpecialties,
 );
 
-router.delete('/:id', SpecialtiesControllers.deleteSpecialtiesById);
+router.delete(
+    '/:id',
+    auth('ADMIN', 'SUPER_ADMIN'),
+    SpecialtiesControllers.deleteSpecialtiesById,
+);
 
 export const SpecialtiesRoutes = router;
