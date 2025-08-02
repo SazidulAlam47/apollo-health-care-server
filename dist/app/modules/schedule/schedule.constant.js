7aa81e85525433bf738e6b432cb9acc97946ff7e
Sazidul Alam <sazidulalam20@gmail.com>
2025-08-02 11:41:30 +0600

    Delete dist directory

--- dist/app/modules/schedule/schedule.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.scheduleFilters = void 0;
-exports.scheduleFilters = [
-    'startDateTime',
-    'endDateTime',
-];
--- dist/app/utils/pad.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pad = (num) => String(num).padStart(2, '0');
-exports.default = pad;
